142d0f08e4d4b538fee4a8693db9a4b1f9e6c825
luches02 <ochulo02@byu.edu>
2024-03-08 22:15:41 -0700

    changed spacing

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,24 @@ interface TeamProps {
 }
 
 function Welcome() {
-  return <h1>List of College Basketball Teams</h1>;
+  return (
+    <div>
+      <h1>List of College Basketball Teams</h1>
+      <br />
+      <h3>
+        This is a website that lists out information about all the colleges in
+        NCAA Basketball.
+      </h3>
+      <br></br>
+    </div>
+  );
 }
 
 class Team extends React.Component<TeamProps> {
   render() {
     const oneTeam = this.props;
     return (
-      <div>
+      <div className="team-card">
         <h2>{oneTeam.school}</h2>
         <h3>Mascot Name: {oneTeam.name}</h3>
         <h3>Location: {oneTeam.city + ', ' + oneTeam.state}</h3>
