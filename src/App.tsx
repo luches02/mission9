f0fa80a6c73598455574803a6eabb9887ad07ed0
luches02 <ochulo02@byu.edu>
2024-03-08 18:16:29 -0700

    built mission9 requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamsObject from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+// const bandNames = [
+//   {
+//     name: 'The Beastie Boys',
+//     members: 'Ad Rock, MCA, Mike D',
+//     formed: 1979,
+//   },
+//   {
+//     name: 'Wolfmother',
+//     members: 'Andrew Stockdale, Hamish Rosser, Bobby Poulton',
+//     formed: 2004,
+//   },
+//   {
+//     name: 'Cream',
+//     members: 'Eric Clapton, Jack Bruce, Ginger Baker',
+//     formed: 1966,
+//   },
+//   {
+//     name: 'Nirvana',
+//     members: 'Kurt Cobain, Dave Groul, Krist Novoselic',
+//     formed: 1987,
+//   },
+// ];
+
+function Welcome() {
+  return <h1>List of College Basketball Teams</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot Name: {oneTeam.name}</h3>
+        <h3>Location: {oneTeam.city + ', ' + oneTeam.state}</h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamsObject.teams.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
